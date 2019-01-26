d3c107ad1279aa11e5e2c6d8487c91095a66d295
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-01-26 14:30:56 +0900

    Add project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenLibraryList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
